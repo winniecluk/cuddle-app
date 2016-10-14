f54c5e8530fa5c1cfac03ac651b2f27b23514529
Winnie Luk <winniecluk@gmail.com>
2016-10-14 13:36:52 -0700

    create data

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// data
+var questions;
+var responses;
+var images;
+
+  // Questions
+questions = ["Where would you like to cuddle?",
+"What would you like to snacking on while you cuddle?",
+"Who would you like to cuddle with?",
+"What do you want to say to her?"]
+
+
+  // Responses from Winnie
+responses = ["That's nice, dear. Why don't you go eat some pie?",
+"How could you say that to me with a straight face?",
+"Wouldn't you rather do something more interesting? ;)"]
+
+  // image links
+images = [
+  {
+    image1: "link",
+    image2: "link2",
+    image3: "link3"
+  },
+  {
+    image1: "link",
+    image2: "link2",
+    image3: "link3"
+  }
+  {
+    image1: "link",
+  }
+]
