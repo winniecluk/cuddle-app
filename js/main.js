4c8d0f0f3f1fc8085bd747504927f64a1178bbc4
Winnie Luk <winniecluk@gmail.com>
2016-10-14 20:52:11 -0700

    create successful transition function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // data
+window.onload = function() {
 var questions;
 var choices;
 var responses;
@@ -13,19 +14,19 @@ questions = ["Where would you like to cuddle?",
   // Choices
 choices = [
   {
-    place1: "on the couch",
-    place2: "on the floor",
-    place3: "on the bed (ohohohoho)"
+    item1: "on the couch",
+    item2: "on the floor",
+    item3: "on the bed (ohohohoho)"
   },
   {
-    snack1: "Cheezits",
-    snack2: "Doritos",
-    snack3: "fruit and vegetable plate -- I'm healthy!"
+    item1: "Cheezits",
+    item2: "Doritos",
+    item3: "fruit and vegetable plate -- I'm healthy!"
   },
   {
-    person1: "Winnie",
-    person2: "Winnie (hint: there might be a reason why I appear more than once)",
-    person3: "Paris Hilton"
+    item1: "Winnie",
+    item2: "Winnie (hint: there might be a reason why I appear more than once)",
+    item3: "Paris Hilton"
   },
 ];
 
@@ -55,15 +56,19 @@ images = [
 
 // controller
   // when you click on Cuddle With Me button
+  // Cuddle with me button will be replaced by next button
   // question 1 will pop up
+  // three choices will pop up
 
 
+// once I have window.onload, these variables are no longer available in the console
   // eventlistener, will run function that makes everything disappear and question 1 pop up
 var startButton = document.querySelector('#start-button');
 var questionWindow = document.querySelector('.question-window');
 var outputWindow = document.querySelector('.output-window');
 var buttonWindow = document.querySelector('.button-window');
-
+var answerWindow = document.querySelector('.answer-window');
+var question1Choices = document.querySelector('#question1choices');
 
 
 // new elements
@@ -71,21 +76,53 @@ var nextButton = document.createElement('button');
 var nextText = document.createTextNode('Next');
 nextButton.appendChild(nextText);
 
-startButton.addEventListener('click', changeStartButton);
+startButton.addEventListener('click', transition);
 
 // this function inserts the next button into the page. it will only be run once
-function changeStartButton(evt){
-  disappear();
+function transition(evt){
+  questionAppear(0);
+  changeStartButton();
+  insertChoices(question1Choices, 0);
+}
+
+// this function makes the Start button in the button window disappear and the next button reappear. This will only happen once
+function changeStartButton(){
+  buttonWindow.innerHTML = '';
   buttonWindow.appendChild(nextButton);
 }
 
-// this function makes the button in the button window disappear
-function disappear(){
-  buttonWindow.innerHTML = '';
+  // make question 1 pop up
+function questionAppear(idx){
+  questionWindow.innerHTML = questions[idx];
+}
+
+// if I didn't have window.onload, this would be null instead of returning a document-fragment
+// function insertChoices(){
+//   // you can create a clone first and then modify the clone and append
+//   // or modify the original template and then clone and append the clone
+//   // returns array of Nodes (spans)
+//   var span = question1Choices.content.querySelectorAll('span');
+//   span[0].textContent = choices[0].item1;
+//   span[1].textContent = choices[0].item2;
+//   span[2].textContent = choices[0].item3;
+//   // importNode creates copy of node from possibly external doc that can be inserted into current doc
+//   var clone = document.importNode(question1Choices.content, true);
+//   answerWindow.appendChild(clone);
+// }
+
+function insertChoices(templateID, idx){
+  var span = templateID.content.querySelectorAll('span');
+  // for (var i = 0; i < span.length; i++)
+  span[0].textContent = choices[idx].item1;
+  span[1].textContent = choices[idx].item2;
+  span[2].textContent = choices[idx].item3;
+  var clone = document.importNode(templateID.content, true);
+  answerWindow.appendChild(clone);
 }
 
 
-  // make question 1 pop up
+
+}; //onload
 
   // you get asked question 1 about places
   // answer1 will produce placeImage1, answer 2 will produce placeImage2, etc.
